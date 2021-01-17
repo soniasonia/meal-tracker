e319c226ee1d009bc51c5153cab3154ee08e16e7
sonia <pawlaksonia@gmail.com>
2021-01-17 11:19:00 +0100

    db user for app can create test database

--- db/postgres-init.sh
+++ db/postgres-init.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER docker PASSWORD 'supersecretpassword';
+    CREATE USER docker PASSWORD 'supersecretpassword' NOSUPERUSER CREATEDB;
     CREATE DATABASE backend;
     GRANT ALL PRIVILEGES ON DATABASE backend TO docker;
 EOSQL
